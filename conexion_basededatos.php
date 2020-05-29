43a7bdc2f86e79f6f3998aeb2cc778402b1685d0
VzqzAlfred <alfred.vzqz96@outlook.com>
2020-05-28 19:42:38 -0500

    asdfgh

--- conexion_basededatos.php
+++ conexion_basededatos.php
@@ -4,7 +4,7 @@
     die("Problemas en la conexión");
 
 
-    mysql_select_db("prueba", $conectar) or 
+    mysqli_select_db("prueba", $conectar) or 
     die('Problemas en laseleccion base de datos');
 
     //Recuperamos las variables crando con $ 
